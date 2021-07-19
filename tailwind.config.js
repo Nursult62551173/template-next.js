98ca5f1c3ca463b9b56375a7a7245a7fa052211e
Jonathan Clem <jonathan@jclem.net>
2021-07-19 15:38:39 -0400

    Remove theme extensions from Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,7 @@ module.exports = {
   mode: 'jit',
   purge: ['./styles/**/*', './components/**/*', './pages/**/*'],
   theme: {
-    extend: {
-      fontFamily: {
-        sans:
-          'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", Arial, sans-serif',
-        mono:
-          'ui-monospace, Menlo, Monaco, "Cascadia Mono", "Segoe UI Mono", "Roboto Mono", "Oxygen Mono", "Ubuntu Monospace", "Source Code Pro", "Fira Mono", "Droid Sans Mono", "Courier New", monospace'
-      }
-    }
+    extend: {}
   },
   variants: {},
   plugins: []
